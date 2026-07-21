eac8b0a259619d56e410c3320ff689fdaa7eb36b
srinivas-irri <srinuvas.irri@gmail.com>
2026-07-21 21:13:33 +0530

    removed localhost in api js

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,6 @@
 const API_URL =
-    import.meta.env.VITE_API_URL || "http://localhost:5000";
+    // import.meta.env.VITE_API_URL || "http://localhost:5000";
+    import.meta.env.VITE_API_URL;
 
 console.log("API_URL =", API_URL);
 
